7ec5c4f5edb098e0c8ced01129fdc09a11149e9d
Эльмира <capricorn_26@mail.ru>
2020-08-23 02:31:19 +0700

    Исправляет отображение иконок типа поездки

--- src/view/event.js
+++ src/view/event.js
@@ -42,7 +42,7 @@ export const createEventTemplate = (event) => {
     `<li class="trip-events__item">
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${getPreposition(type)} ${destination}</h3>
 
