a9ab2b1421ba319644d5c178072b72de9134cd33
Эльмира <capricorn_26@mail.ru>
2020-09-27 22:49:52 +0700

    Б12

--- src/view/add-new-event-button.js
+++ src/view/add-new-event-button.js
@@ -1,5 +1,5 @@
 import AbstractView from "./abstract.js";
-import {MenuItem} from "./../const.js";
+import {MenuItem} from "../const.js";
 
 export default class AddNewEventButton extends AbstractView {
   constructor() {
